488d5913ba76d5d5ca31af856a87ff0fdd370674
wharmond <43218226+wharmond@users.noreply.github.com>
2023-05-11 12:37:12 -0400

    combobox test 1

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function clearScreen() {
     document.getElementById("result").value = "";
 }
 
-
 // This function display values
 function display(value) {
     document.getElementById("result").value += value;
@@ -15,3 +14,19 @@ function calculate() {
     var q = eval(p);
     document.getElementById("result").value = q;
 }
+
+function getCity() {
+  var x = document.getElementById("country").value;
+  var items;
+  if (x === "uk") {
+    items = ["London", "Leeds", "Glasgow"];
+  } else {
+    items = ["New York City", "Ottawa", "Singapore"]
+  }
+  var str = ""
+  for (var item of items) {
+    str += "<option>" + item + "</option>"
+  }
+  document.getElementById("pickone").innerHTML = str;
+}
+document.getElementById("country").addEventListener("onchange", getType)
